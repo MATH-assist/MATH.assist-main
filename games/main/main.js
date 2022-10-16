d1cc512343bb0c015f60653b5c751656577410c4
Sean <83432253+MegaTheLEGEND@users.noreply.github.com>
2022-10-15 18:18:47 -0700

    Update main.js

--- games/main/main.js
+++ games/main/main.js
@@ -2,3 +2,19 @@ const newTitle = localStorage.getItem('storedtitle');
 
 document.title = newTitle;
 
+
+const changeTabTitle = () => {
+    const newtitle = document.getElementById("userinput");
+    if (newtitle.value == ""){ //check if the input is blank when they submit
+        window.localStorage.removeItem("title");
+        window.document.title = "lEGEND"
+        document.getElementById("console-output").style.color = "red"; //error = red
+        console_output.innerText = "No title entered. Default applied" //return output successful
+    } else {
+        window.localStorage.setItem("title", newtitle.value);
+        window.document.title = newtitle.value; //Set window's title to userinput
+        document.getElementById("console-output").style.color = null; //reset output's color to green
+        console_output.innerText = "Title change successful" //return output successful
+    }
+    newtitle.value = ""; //clear input
+};
