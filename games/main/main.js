3f532390339ac304f27482addd75369f01084348
Sean <83432253+MegaTheLEGEND@users.noreply.github.com>
2022-10-15 21:02:46 -0700

    Update main.js

--- games/main/main.js
+++ games/main/main.js
@@ -40,11 +40,11 @@ const resetTabSettings = () => {
     window.localStorage.setItem("storedtitle", "LEGEND");//set default site name
     window.location.reload();
 };
-/*
+
 //URL Validation Regex
 const validURL = (str) => {
     var expression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
     var regex = new RegExp(expression);
     return !!regex.test(str);
 }
-*/
+
