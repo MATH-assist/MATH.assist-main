cfc11a5de8b3ddce7dd1b8f415e630dd0298f846
Sean <83432253+MegaTheLEGEND@users.noreply.github.com>
2022-10-16 14:58:56 -0700

    Update main.js

--- games/main/main.js
+++ games/main/main.js
@@ -8,6 +8,7 @@ document.title = currentTitle;
 window.addEventListener('load', (event) => {
   console.log('page is fully loaded now atempting to restore favicon and add a site name');
      changeFavicon(currentIcon);
+  storedtitle = storedtitle === undefined ? 'LEGEND' : storedtitle;
  // if (storedtitle === 'undefined') {
    //     window.document.title = "LEGEND"
 //  }else{
